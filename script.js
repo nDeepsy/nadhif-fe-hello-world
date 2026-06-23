ab5362be3f16205ed4e0b6a49cbf24570486cc0e
Nadhif Faiz Al Fahrezy <nadhiffaizalfahrezy@gmail.com>
2026-06-23 22:19:26 +0700

    feat: tambah render awal aplikasi

---
+++ script.js
@@ -0,0 +1,8 @@
+const app = document.querySelector("#app");
+
+app.innerHTML = `
+  <section class="intro">
+    <h1>Informatika 3D</h1>
+    <p>Media pembelajaran interaktif SMPN 2 Tasikmalaya.</p>
+  </section>
+`;
